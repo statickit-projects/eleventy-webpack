1b38a011e139f359aacd22cc93e0e220632952eb
Derrick Reimer <derrickreimer@gmail.com>
2020-02-12 10:21:14 -0600

    Small tweaks

--- .eleventy.js
+++ .eleventy.js
@@ -2,24 +2,25 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = function(eleventyConfig) {
-  const output = '_site';
+  const outputDir = '_site';
+  const assetDir = 'assets';
 
   eleventyConfig.addFilter('assetPath', function(value) {
     if (process.env.ELEVENTY_ENV === 'production') {
       const manifestPath = path.resolve(
         __dirname,
-        output,
-        'assets',
+        outputDir,
+        assetDir,
         'manifest.json'
       );
       const manifest = JSON.parse(fs.readFileSync(manifestPath));
-      return `/assets/${manifest[value]}`;
+      return `/${assetDir}/${manifest[value]}`;
     }
 
-    return `/assets/${value}`;
+    return `/${assetDir}/${value}`;
   });
 
   return {
-    dir: { input: 'src', output }
+    dir: { input: 'src', output: outputDir }
   };
 };
