c8745349845f2d6ba5a03a4ffa742c84bc27073a
Derrick Reimer <derrickreimer@gmail.com>
2020-02-12 08:10:30 -0600

    Implement asset fingerprinting

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = function(eleventyConfig) {
+  const output = '_site';
+
+  eleventyConfig.addFilter('assetPath', function(value) {
+    if (process.env.ELEVENTY_ENV === 'production') {
+      const manifestPath = path.resolve(
+        __dirname,
+        output,
+        'assets',
+        'manifest.json'
+      );
+      const manifest = JSON.parse(fs.readFileSync(manifestPath));
+      return `/assets/${manifest[value]}`;
+    }
+
+    return `/assets/${value}`;
+  });
+
   return {
-    dir: { input: 'src', output: '_site' }
+    dir: { input: 'src', output }
   };
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
+const ManifestPlugin = require('webpack-manifest-plugin');
 
-module.exports = {
-  entry: './src/scripts/main.js',
-  output: {
-    path: path.resolve(__dirname, '_site/assets'),
-    filename: 'main.js'
-  }
+module.exports = (_env, argv) => {
+  return {
+    entry: './src/scripts/main.js',
+    output: {
+      path: path.resolve(__dirname, '_site/assets'),
+      filename:
+        argv.mode === 'production' ? '[name].[contenthash].js' : '[name].js'
+    },
+    plugins: [new ManifestPlugin()]
+  };
 };
